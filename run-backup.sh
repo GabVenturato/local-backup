fa348100fffaa0f4c754c3ae50668bab7c306ce7
Gabriele Venturato <venturato.gabriele@gmail.com>
2018-08-15 00:35:24 +0200

    added the possibility to clean repository after backup + minor fixes

--- run-backup.sh
+++ run-backup.sh
@@ -8,7 +8,17 @@
 FILESFROM=include-file.txt
 FILESEXCLUDE=exclude-file.txt
 PWDFILE=password.txt
+KEEPLAST=10
 
+# Function used to check the repository integrity after each operation.
+# @in: string to print before the check
+check_repo () {
+    echo $1
+    restic -r $REPO --password-file=$PWDFILE check
+    echo $'\n\n'
+}
+
+# ------------------------------------ MAIN ------------------------------------
 if [ "$#" -ne 1 ]; then
     echo "usage: run-backup.sh <restic-repo-path>"
     exit 1
@@ -24,23 +34,34 @@ if [ "$?" -ne 0 ]; then
 fi
 
 # CHECK BEFORE BACKUP
-echo "Checking repository before backup..."
-restic -r $REPO --password-file=$PWDFILE check
-echo
-echo
+check_repo "-> Checking repository before backup..."
 
 # DO BACKUP
+echo "-> Doing the backup..."
 restic -r $REPO backup --verbose \
 --password-file=$PWDFILE \
 --files-from $FILESFROM \
 --exclude-file=$FILESEXCLUDE \
 2> errors.log.txt
-echo
-echo
+echo $'\n\n'
 
 # CHECK AFTER BACKUP
-echo "Checking repository after backup..."
-restic -r $REPO --password-file=$PWDFILE check
+check_repo "-> Checking repository after backup..."
 
+# WANT TO CLEAN?
+echo -n "Do you want to clean the repository (keep last $KEEPLAST)? [y/N]: "
+read choice junk
+echo $'\n\n'
 
+if test "$choice" = y -o "$choice" = Y
+then
+    echo "-> Cleaning..."
+    restic -r $REPO forget --password-file=$PWDFILE \
+    --keep-last $KEEPLAST --prune
+    echo $'\n\n'
+    check_repo "-> Checking repository after cleaning..."
+fi
 
+# PRINT CURRENT SNAPSHOTS
+echo "-> Current Snapshots:"
+restic -r $REPO --password-file=$PWDFILE snapshots
