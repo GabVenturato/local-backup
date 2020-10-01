876ab9d01b67c60ddbf1fe9934e2b25283f8bd01
GabVenturato <venturato.gabriele@gmail.com>
2020-10-02 00:22:50 +0200

    Small fixes and new cli option to accept requests by default

--- run-backup.sh
+++ run-backup.sh
@@ -9,6 +9,19 @@ FILESFROM=include-file.txt
 FILESEXCLUDE=exclude-file.txt
 PWDFILE=password.txt
 KEEPLAST=10
+DEFAULTY=false
+
+# Function used to print the help
+help() {
+  cat <<HELP
+Usage: ./$(basename $0) <restic-repo-path> [options]
+
+Run the restic backup routine.
+
+Options:
+  -y | --yes        To accept all requests by default.
+HELP
+}
 
 # Function used to check the repository integrity after each operation.
 # @in: string to print before the check
@@ -19,8 +32,27 @@ check_repo () {
 }
 
 # ----------------------------------- MAIN ----------------------------------- #
+# Optional parameters
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -y|--yes)
+      DEFAULTY=true
+      shift
+      ;;
+    *) # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift
+      ;;
+  esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# Mandatory parameters
 if [ "$#" -ne 1 ]; then
-    echo "usage: run-backup.sh <restic-repo-path>"
+    help
     exit 1
 fi
 
@@ -29,7 +61,7 @@ REPO=$1
 # check if path in input is a restic repo
 restic -r $REPO snapshots --password-file=$PWDFILE &> /dev/null
 if [ "$?" -ne 0 ]; then
-    echo "error: $REPO is not a restic repository"
+    echo "error: $REPO could be not a restic repository, try with a check"
     exit 1
 fi
 
@@ -49,12 +81,13 @@ echo $'\n\n'
 check_repo "-> Checking repository after backup..."
 
 # WANT TO CLEAN?
-echo -n "Do you want to clean the repository (keep last $KEEPLAST)? [y/N]: "
-read choice junk
-echo $'\n\n'
+if [[ ! $DEFAULTY ]]; then
+    echo -n "Do you want to clean the repository (keep last $KEEPLAST)? [y/N]: "
+    read choice junk
+    echo $'\n\n'
+fi
 
-if test "$choice" = y -o "$choice" = Y
-then
+if [[ $DEFAULTY || "$choice" = y || "$choice" = Y ]]; then
     echo "-> Cleaning..."
     restic -r $REPO forget --password-file=$PWDFILE \
     --keep-last $KEEPLAST --prune
